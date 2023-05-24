3e91214df86ee8aa47a86f633ded34df25b1364f
Mattias Hjerpe <mattiash91@gmail.com>
2023-05-24 10:21:32 +0200

    Start

--- src/main/java/org/example/WebpageOpener.java
+++ src/main/java/org/example/WebpageOpener.java
@@ -2,19 +2,29 @@ package org.example;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class WebpageOpener {
     static WebDriver driver;
     public static void main(String[] args) {
+        startUp();
+    }
+
+    public static void startUp() {
         // Set the path to the chromedriver executable
         System.setProperty("webdriver.chrome.driver", "src/main/java/chromedriver.exe");
 
-        // Create a new instance of the ChromeDriver
-        driver = new ChromeDriver();
+        // Create ChromeOptions instance
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");  // Add argument to maximize the browser window
+
+        // Create a new instance of the ChromeDriver with ChromeOptions
+        WebDriver driver = new ChromeDriver(options);
 
         // Open a webpage
         driver.get("https://www.ltu.se");
 
+        // Selenium
         selenium();
     }
 
