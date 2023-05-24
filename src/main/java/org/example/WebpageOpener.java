c351414f42f9fe9524f191455d42d964aeb67650
Mattias Hjerpe <mattiash91@gmail.com>
2023-05-24 10:25:33 +0200

    Start

--- src/main/java/org/example/WebpageOpener.java
+++ src/main/java/org/example/WebpageOpener.java
@@ -1,11 +1,18 @@
 package org.example;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.io.File;
+import java.io.IOException;
+
 public class WebpageOpener {
     static WebDriver driver;
+    static String email = "";
+    static String password = "";
     public static void main(String[] args) {
         startUp();
     }
@@ -24,8 +31,27 @@ public class WebpageOpener {
         // Open a webpage
         driver.get("https://www.ltu.se");
 
+        //json
+        jsonCredentials();
+
         // Selenium
         selenium();
+    }
+
+    public static void jsonCredentials(){
+        try {
+            // Locate the json file
+            File jsonFile = new File("C:\\temp\\ltu.json");
+            ObjectMapper objectMapper = new ObjectMapper();
+            // Read the json file
+            JsonNode jsonNode = objectMapper.readTree(jsonFile);
+
+            // Get the email and password from the json file
+            email = jsonNode.get("ltuCredentials").get("email").asText();
+            password = jsonNode.get("ltuCredentials").get("password").asText();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
     }
 
     public static void selenium(){
